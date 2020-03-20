b372664a8b907f778aeac41487ced6a140e20df6
lephuc3108 <lephuc3108@gmail.com>
2020-03-20 16:02:32 +0700

    update settings

--- personalsite/personalsite/settings.py
+++ personalsite/personalsite/settings.py
@@ -128,7 +128,7 @@ INTERNAL_IPS = ('127.0.0.1',)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
